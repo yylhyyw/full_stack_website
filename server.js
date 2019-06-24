e10d034ea28486a0b43641c3a762a4329a1d16ca
yylhyyw <yylhyyw@gmail.com>
2019-06-24 18:36:22 -0400

    init

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+// Get our API routes
+
+const app = express();
+
+var indexRouter = require('./server/routes/api');
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set our api routes
+app.use('/api', indexRouter);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
